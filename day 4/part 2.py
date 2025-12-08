b7e1d1fd889181a0be53c4455d5e636b3242f729
Caleb Hansen <145716875+calebh13@users.noreply.github.com>
2025-12-07 19:17:02 -0800

    Create part 2.py

---
+++ day 4/part 2.py
@@ -0,0 +1,39 @@
+INPUT_FILE = r"day 4/input.txt"
+# INPUT_FILE = r"day 4/test.txt"
+
+NEIGHBORS = (
+    (-1, -1), (-1, 0), (-1, 1),
+    ( 0, -1),          ( 0, 1),
+    ( 1, -1), ( 1, 0), ( 1, 1),
+)
+
+if __name__ == "__main__":
+    idSum = 0
+    grid = []
+    def adjacentRolls(row: int, col: int) -> int:
+        count = 0
+        for dX, dY in NEIGHBORS:
+            nRow = row + dY
+            if 0 <= nRow < len(grid):
+                nCol = col + dX
+                if 0 <= nCol < len(grid[nRow]) and grid[nRow][nCol] == '@':
+                    count += 1
+        return count
+
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            grid.append(line)
+    
+    totalRemoved = 0
+    while True:
+        removed = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == '@' and adjacentRolls(row, col) < 4:
+                    grid[row] = grid[row][:col] + '.' + grid[row][col+1:]
+                    removed += 1
+        totalRemoved += removed
+        if removed == 0:
+            break
+    
+    print(totalRemoved)
